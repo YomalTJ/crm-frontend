846ea766885a70c5d40becb4fb85eabe2754c6ad
yomal-at-275750982137 <yomalthushara2295266@gmail.com>
2025-07-17 10:48:14 +0530

    add samurdhi from to department dashboard

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,14 +1,9 @@
-import SamurdhiFamillyForm from '@/components/forms/SamurdhiFamillyForm'
 import React from 'react'
 
 const AdminDashboard = () => {
   return (
     <div>
       <h1 className='text-3xl text-white font-semibold'>Admin Dashboard</h1>
-
-      <div className="space-y-6 mt-10">
-        <SamurdhiFamillyForm />
-      </div>
     </div>
   )
 }
