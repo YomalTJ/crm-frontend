9d363e08dc412b8427d21f2696ac88063cd138af
YomalTJ <yomalt@erabiz.io>
2025-07-21 14:56:25 +0530

    adjust user dropdown

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
-import { DropdownItem } from "../ui/dropdown/DropdownItem";
+// import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { logoutUser } from "@/services/authService";
 import { useRouter } from "next/navigation";
 
@@ -80,7 +80,7 @@ const handleSignOut = async () => {
           </span>
         </div>
 
-        <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-gray-200 dark:border-gray-800">
+        {/* <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-gray-200 dark:border-gray-800">
           <li>
             <DropdownItem
               onItemClick={closeDropdown}
@@ -156,7 +156,7 @@ const handleSignOut = async () => {
               Support
             </DropdownItem>
           </li>
-        </ul>
+        </ul> */}
         <button
           onClick={handleSignOut}
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
