b1711bb241f6b99bdbb4be6790a7e7a012282b0b
YomalTJ <yomalt@erabiz.io>
2025-07-21 11:12:05 +0530

    adjust user details

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -75,10 +75,10 @@ const handleSignOut = async () => {
       >
         <div>
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-            Musharof Chowdhury
+            Yomal Thushara
           </span>
           <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
-            randomuser@pimjo.com
+            yomal@gmail.com
           </span>
         </div>
 
