b7a4eb77646830ab989ea89f09c21aa34e1a6f7b
YomalTJ <yomalt@erabiz.io>
2025-07-21 10:32:40 +0530

    added logout redirections

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,6 +1,14 @@
+import { getCurrentUser } from '@/lib/auth';
 import React from 'react'
+import { redirect } from "next/navigation";
 
-const AdminDashboard = () => {
+const AdminDashboard = async () => {
+  const user = await getCurrentUser();
+  
+    if (!user || user.role !== 'user') {
+        redirect('/signin')
+      }
+
   return (
     <div>
       <h1 className='text-3xl text-white font-semibold'>Admin Dashboard</h1>
--- src/app/dashboard/operations-supervisor/page.tsx
+++ src/app/dashboard/operations-supervisor/page.tsx
@@ -1,6 +1,14 @@
+import { getCurrentUser } from '@/lib/auth';
 import React from 'react'
+import { redirect } from "next/navigation";
 
-const OperationSupervisorDashboard = () => {
+const OperationSupervisorDashboard = async () => {
+  const user = await getCurrentUser();
+
+  if (!user || user.role !== 'user') {
+      redirect('/signin')
+    }
+
   return (
     <div>
       <h1 className='text-3xl text-white font-semibold'>Operation Supervisor Dashboard</h1>
