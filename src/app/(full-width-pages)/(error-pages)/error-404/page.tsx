4edd15c18b873dc4fb2712733fdc70826abcd168
yomal-at-275750982137 <yomalthushara2295266@gmail.com>
2025-10-23 08:04:53 +0530

    updated

--- src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Error 404 | TailAdmin - Next.js Dashboard Template",
+  title: "Samurdhi Department Error 404 | Samurdhi Department",
   description:
-    "This is Next.js Error 404 page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "This is Samurdhi Department Error 404 page",
 };
 
 export default function Error404() {
